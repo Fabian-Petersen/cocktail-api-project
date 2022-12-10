8e06b96a1d15d5ec0bdc334830d3084ff7d46198
Fabian Petersen <fabian@webrus.co.za>
2022-12-10 09:40:13 +0200

    file-location-and-about-updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,14 +3,21 @@ import React from "react";
 const About = () => {
   return (
     <section className="section about-section">
-      <h1 className="section-title">About Us</h1>
+      <h1 className="section-title">About</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non
-        nulla sit amet nisl tempus convallis quis ac lectus. Cras ultricies
-        ligula sed magna dictum porta. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Proin eget tortor risus. Cras ultricies ligula sed
-        magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et,
-        porttitor at sem. Donec sollicitudin molestie malesuada.
+        A project created with React to show of various cocktails. The
+        information is from the public API "thecocktaildb.com". The skills
+        showcased in this projects is as follows:
+      </p>
+      <ul>
+        <li> Fetch Data from API using Axios library</li>
+        <li> CReate Search Functionality for a single product.</li>
+        <li> Use of React Router version 6</li>
+      </ul>
+      <p>
+        It was fun to create the project and work on the functionality to
+        improve my skills as a REACT Front End Developer. Further updates will
+        be made to improve the UI/UX of the site,...Stay tuned!!
       </p>
     </section>
   );
